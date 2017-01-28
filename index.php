dc5e8f1b2e7cc5f52868bb3fa3a784ccafdcb811
MelissaMejia <melissa.gms93@gmail.com>
2017-01-28 11:03:22 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Politicas de privacidad</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+<style type="text/css">
+     html, body {
+   margin right: 20px;
+    margin left: 20px;}
+    </style>
+     </head>
+<body BGCOLOR="#F4FAFB">
+<h1>Politicas de privacidad</h1>
+1. LA INFORMACIÓN QUE RECOPILAMOS Y CÓMO LA USAMOS</br></br>
+A continuación describimos los tipos de información que recopilamos y cómo se utiliza. 
+<ul>
+<li><b>Datos sobre tu ubicación física</b></li>
+<p>La aplicación recopila y procesa información sobre tu ubicación real. Empleamos diferentes tecnologías para determinar la ubicación, como el sistema GPS y el uso de otros sensores que pueden proporcionar dicha información, por ejemplo, puntos de acceso Wi-Fi y antenas de telefonía móvil.</p>
+
+<li><b>Almacenamiento local</b></li>
+<p>La aplicación almacena fotos en su memoria interna para que puedan ser subidas al servidor.</p>
+<li><b>Utilización de la cámara</b></li></br>
+La aplicación necesita el permiso de utilizar su camara para poder tomar las fotos del lugar que desea tomar.
+<p>
+</p>
+</ul>
+
+
+2. SEGURIDAD</br></br>
+Estamos preocupados por salvaguardar la confidencialidad de su información. Proveemos
+salvaguardas físicas, electrónicas y de procedimiento para proteger la información que
+procesamos y mantener. Por ejemplo, limitamos el acceso a esta información a los
+empleados y contratistas autorizados que necesitan conocer dicha información para operar,
+desarrollar o mejorar nuestra aplicación. Tenga en cuenta que, aunque nos esforzamos por
+proporcionar seguridad razonable de información que procesamos y mantenemos, ningún
+sistema de seguridad puede prevenir todas las brechas de seguridad potenciales.</br></br>
+
+3. SU CONSENTIMIENTO</br></br>
+Mediante el uso de la aplicación, usted acepta nuestro procesamiento de sus datos en los
+términos de esta Política de Privacidad de vez en cuando modificado por nosotros.
+"Proceso", significa el uso de cookies en un dispositivo de computadora y/o portátil o tocar
+la información de cualquier manera, no limitado a recoger, almacenar, borrar, utilizar la
+combinación y la divulgación de la información, todas las actividades que se llevarán a
+cabo en PERÚ.</br></br>
+
+4. CONTÁCTENOS</br></br>
+Si usted tiene alguna pregunta respecto a la privacidad durante el uso de la aplicación, o si
+tiene preguntas sobre nuestras prácticas, por favor póngase en contacto con nosotros por
+correo electrónico a OPERACIONES@GIIS.COM.PE
+  </body>
+</html>
